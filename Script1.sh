4b9821055c242e75c93529641961b13f96c36b06
shubhamWithCode <121483086+shubhamWithCode@users.noreply.github.com>
2024-01-10 17:34:48 +0530

    Update Script1.sh

--- Script1.sh
+++ Script1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#This is my 1st shell script to print output
+#This is my 1st shell script to print op
 echo "Hello All, Welcome to shubhams repo"
 echo "Hello, How are you?"
 echo "fine"
