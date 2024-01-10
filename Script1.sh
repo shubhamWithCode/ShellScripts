750ad794b7662e72b03222a2d2c8d2f99b5acc60
shubham <vickygaikwad41996@gmail.com>
2024-01-10 17:33:12 +0530

    shubham commiting changes in 1,4,5

--- Script1.sh
+++ Script1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #This is my 1st shell script to print output
-echo "Hello All, Welcome to AWS/Devops Class"
+echo "Hello All, Welcome to shubhams repo"
 echo "Hello, How are you?"
-echo "Welcome to Young Minds"
-echo "Hello batch-21, We are learning Jenkins"
+echo "fine"
+echo "Hello, We are learning shellscripts"
 
